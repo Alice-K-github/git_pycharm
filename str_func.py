e41a7197ba5290968876cb7823f8b062164134ee
Alice-K-github <Grayhild@mail.ru>
2024-01-26 21:15:31 +0700

    str_func.py изменения (локал)

--- str_func.py
+++ str_func.py
@@ -1,3 +1,16 @@
+from turtle import title
+
+#Функция
 def dunc(user_input):
+    #Ввод строки пользователем
     user_input = input()
+    #Вывод строки заглавными буквами
     return print(user_input.upper())
+
+
+#Функция 2
+def func_2(alf):
+    #Ввод строки пользователем
+    alf = input()
+    #Вывод строки, где каждое слово будет с заглавной буквы
+    return print(title(alf))
